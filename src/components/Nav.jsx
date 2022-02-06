1362bd6c14568adead9097a93029c65796b29e83
Alexander-Lake <69411995+Alexander-Lake@users.noreply.github.com>
2022-02-06 02:25:37 -0600

    create navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 
+function boxSlide() {
+  const box = document.querySelector(".animation");
+}
+
 export function Nav() {
   return (
     <div className="nav-container">
-      <ul className="menu">
-        <li className="two">
-          <span>Home</span>
-        </li>
-        <li className="three">
-          <span>About</span>
-        </li>
-        <li className="four">
-          <span>Skills</span>
-        </li>
-        <li className="five">
-          <span>Projects</span>
-        </li>
-        <li className="one">
-          <span>Contact</span>
-        </li>
-        <div className="animation"></div>
-      </ul>
+      <div className="menu-container">
+        <ul className="menu">
+          <li className="home">
+            <span>Home</span>
+          </li>
+          <li className="about" onMouseOver={boxSlide}>
+            <span typeof="button">About</span>
+          </li>
+          <li className="skills">
+            <span>Skills</span>
+          </li>
+          <li className="projects">
+            <span>Projects</span>
+          </li>
+          <li className="contact">
+            <span>Contact</span>
+          </li>
+          <div className="animation"></div>
+        </ul>
+      </div>
     </div>
   );
 }
