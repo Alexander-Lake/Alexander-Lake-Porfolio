30d031f724bfa3a07f471195e026d2a7da05d3fc
Alexander-Lake <69411995+Alexander-Lake@users.noreply.github.com>
2022-02-07 03:00:29 -0600

    re-worked navbar

---
+++ Lookup.js
@@ -0,0 +1,13 @@
+proto._getElementOffset = function( elem ) {
+  var boundingRect = elem.getBoundingClientRect();
+  var thisRect = this._boundingRect;
+  var size = getSize( elem );
+  var offset = {
+    left: boundingRect.left - thisRect.left - size.marginLeft,
+    top: boundingRect.top - thisRect.top - size.marginTop,
+    right: thisRect.right - boundingRect.right - size.marginRight,
+    bottom: thisRect.bottom - boundingRect.bottom - size.marginBottom
+  };
+  return offset;
+};
+document.querySelector("body > div > div.cavani_tm_header > div.menu > ul > li:nth-child(1)")
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,74 @@
-import React from "react";
-
-function boxSlide() {
-  const box = document.querySelector(".animation");
-}
+import React, { useState } from "react";
 
 export function Nav() {
+  const positions = {
+    home: "0%",
+    about: "20%",
+    skills: "40%",
+    projects: "60%",
+    contact: "80%",
+  };
+
+  function handleClick(event) {
+    const box = document.querySelector(".animation");
+    const location = event.target.className;
+    box.style.marginLeft = positions[location];
+  }
+
+  function handleHover(event) {
+    const elem = document.querySelector(".animation");
+    const textSize = event.target.clientWidth;
+    console.log(textSize);
+    Object.assign(elem.style, {
+      borderStyle: "solid",
+      borderWidth: "1px",
+      height: "2px",
+      width: [textSize] + "px",
+    });
+  }
+
   return (
     <div className="nav-container">
       <div className="menu-container">
         <ul className="menu">
           <li className="home">
-            <span>Home</span>
+            <span
+              onMouseOver={handleHover}
+              onClick={handleClick}
+              className="home"
+            >
+              Home
+            </span>
           </li>
-          <li className="about" onMouseOver={boxSlide}>
-            <span typeof="button">About</span>
+          <li className="about" onClick={handleClick}>
+            <span onMouseOver={handleHover}>About</span>
           </li>
           <li className="skills">
-            <span>Skills</span>
+            <span
+              onMouseOver={handleHover}
+              onClick={handleClick}
+              className="skills"
+            >
+              Skills
+            </span>
           </li>
           <li className="projects">
-            <span>Projects</span>
+            <span
+              onMouseOver={handleHover}
+              onClick={handleClick}
+              className="projects"
+            >
+              Projects
+            </span>
           </li>
           <li className="contact">
-            <span>Contact</span>
+            <span
+              onMouseOver={handleHover}
+              onClick={handleClick}
+              className="contact"
+            >
+              Contact
+            </span>
           </li>
           <div className="animation"></div>
         </ul>
