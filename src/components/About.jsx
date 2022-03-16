483d5a1870c308eb9ac95cb6603931203a3fbbc5
Alexander-Lake <69411995+Alexander-Lake@users.noreply.github.com>
2022-03-16 03:01:43 -0500

    updated about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,15 +10,38 @@ export function About() {
         <div className="about-section">
           <div className="summary">
             <p>
-              Hello there! My name is <strong>Alexander Lake</strong> and welcome to the about me section. I am a fullstack developer with a specialization in the MERN stack. I really enjoy building things piece by piece and watching it all come together. I take a mobile first approach to building web applications that are responsive and accessible to many devices. I believe that modern codes should be clean, easy to understand and reusable.  What I love most about software development is the endless opportunities to tackle new challenges. I enjoy learning new things and considers myself a perpetual learner.    
+              Hello there! My name is <strong>Alexander Lake</strong> and
+              welcome to the about me section. I am a fullstack developer with a
+              specialization in the MERN stack, and I enjoy building things that
+              live on the internet. When developing web applications, I always
+              take a mobile first approach to integrate responsiveness and
+              accessibility for all modern devices. I believe that codes should
+              be clean, easy to understand and reusable. What I love most about
+              software development is the endless opportunities to tackle new
+              challenges. I enjoy learning things and considers myself a
+              perpetual learner.
             </p>
             <p>
-              I have 6 years of experience working for a fortune 500 company in finance supporting business bill to cash. My expertise is in process improvement leveraging mordern methods to solve ancient problems. I've worked in a very collaborative environment where groups from many different parts of the business come together to develope new solutions to support the end-user.   
+              I have 6 years of experience working for a fortune 500 company in
+              finance supporting business bill to cash. My current role is Sr.
+              Data Reporting and Analytics. I use data to support decision
+              makers and develope ad hoc reporting solutions. My expertise is in
+              process improvement leveraging mordern methods to solve ancient
+              problems.
             </p>
 
-            <p>I have a masters degree in computer information systems from Colorado State University. This is a STEM program that mainly focused on Data Governance and Data Analytics.</p>
             <p>
-             Now on a more personal level. Feel free to skip to the next section. I am an avid gamer. I love the Massive Multi-player Online (MMO) genre. I enjoy solicizing and working with others to solve challenges in the games. My favorite film genre is Sci-fi, especially the kinds that get you thinking about the unknown. Two of my favorite films of all time are "Arrival" and "Interstellar". This is just a little tidbit about me. If you would like to know more, we can grab some coffee or connect to chat!     
+              I have a masters degree in computer information systems from
+              Colorado State University. The program I completed is a STEM
+              program focusing on Data Governance and Data Analytics.
+            </p>
+            <p>
+              Now on a more personal level. Feel free to skip to the next
+              section. I am a huge gamer. I love the Massive Multi-player Online
+              (MMO) genre. I enjoy solicizing and working with others to solve
+              challenges within the games. My favorite film genre is Sci-fi. Two
+              of my favorite films of all time are "Arrival" and "Interstellar".
+              If you enjoy gaming or love sci-fi as much as I do, lets connect!
             </p>
           </div>
           <div className="education">
